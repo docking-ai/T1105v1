06e2be210002d9cbf5e8c3a4d7747e4765d5b2e8
sness <snessnet@gmail.com>
2022-05-24 04:08:19 -0700

    more

---
+++ 4-dali/copy-relevant-pdb.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+mv pdb1xqp.ent 1xqp.pdb
+mv pdb6g3y.ent 6g3y.pdb
+mv pdb6g40.ent 6g40.pdb
+mv pdb1pu7.ent 1pu7.pdb
+mv pdb1lww.ent 1lww.pdb
+mv pdb1lwv.ent 1lwv.pdb
+mv pdb1m3q.ent 1m3q.pdb
+mv pdb4b21.ent 4b21.pdb
+mv pdb6g40.ent 6g40.pdb
+mv pdb4uob.ent 4uob.pdb
+mv pdb6g40.ent 6g40.pdb
+mv pdb6g3y.ent 6g3y.pdb
+mv pdb1pu8.ent 1pu8.pdb
+mv pdb1hu0.ent 1hu0.pdb
+mv pdb6g3y.ent 6g3y.pdb
+mv pdb1muy.ent 1muy.pdb
+mv pdb3f10.ent 3f10.pdb
